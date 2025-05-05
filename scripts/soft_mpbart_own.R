8443449b15f356a408e1a967beb2afe9da4ad5c2
matthijsnij <matthijsnijeboer@hotmail.nl>
2025-05-05 10:17:51 +0200

    Added new file, own soft mpbart code. Contains at this point a sketch of the MCMC sampler and some other intializations

---
+++ scripts/soft_mpbart_own.R
@@ -0,0 +1,76 @@
+# SOFT MPBART
+
+
+# packages
+library(MASS)
+
+# ------------ READ DATA ---------------
+
+
+# ------------ PREPROCESS DATA -------------
+
+
+
+
+soft_mpbart <- function(y_train, # training data - outcomes
+                        X_train, # training data - covariates
+                        y_test, # test data - outcomes
+                        X_test, # test data - covariates
+                        num_burnin, # number of burn-in iterations
+                        num_sim, # number of simulations (excl. burn-in)
+                        num_trees, # number of trees in the sum-of-trees model
+                        K, # number of outcome categories - 1 (dim of latent vector)
+                        ) {
+  
+  # set some values
+  num_obs_train = length(y_train)
+  num_obs_test = length(y_test)
+  
+  # initialize all model parameters
+  Sigma <- diag(K)
+
+  # ------- MCMC --------
+  for (iter in 1:num_burnin+num_sim) {
+    
+    # sample latent variables from truncated multivariate normal
+    for (i in 1:num_obs_train) {
+      
+    }
+    
+    
+    
+    for (k in 1:K) {
+      
+       # sample all trees, leaf node params and tau via softBART package
+       for (j in 1:num_trees) {
+         
+         
+       }
+      
+      
+       # sample splitting probabilities from Dirichlet, should also come out of softBART
+      
+      
+    }
+    
+    # sample unconstrained Sigma from inverted-Wishart
+    Sigma_star <- 
+    
+    # scale to force trace restriction
+    Sigma <- (K / sum(diag(Sigma_star)))
+    
+    
+    # save draws if after burn-in
+    if (iter > num_burnin) {
+      
+    }
+    
+  }
+  
+  
+  
+}
+
+
+
+
