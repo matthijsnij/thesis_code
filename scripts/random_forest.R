c9193e666ac29868b64adafb4bae1e6bae5650fa
matthijsnij <matthijsnijeboer@hotmail.nl>
2025-06-03 15:23:58 +0200

    Added CV for RF. Added RF in waveform sim. Output for waveform sim is now available for both SMPBART and RF.

--- scripts/random_forest.R
+++ scripts/random_forest.R
@@ -1,7 +1,9 @@
 # RANDOM FOREST
 
 library(randomForest)
+library(caret)
 
+# ---------- RANDOM FOREST -------------
 #'@description Function which uses Random Forest to predict a multiclass response
 #'
 #'@param X_train Matrix of training data - predictors
@@ -27,7 +29,56 @@ rf_multiclass <- function(X_train, y_train, X_test, mtry) {
   # store return object
   return_list <- list()
   return_list$pred_y <- predictions_numeric
-  return_list$probs <- probs
+  return_list$post_probs <- probs
   
   return(return_list)
+}
+
+# ------- RANDOM FOREST CV -----------
+#'@description Cross validation for random forest classifier
+#'
+#'@param X_train Matrix of training data - predictors
+#'@param y_train Vector of training data - responses
+#'@param X_test Matrix of test data - predictors
+#'@param mtry_grid CV grid of mtry values
+#'@param K Number of folds in the CV
+#'@param seed Seed to control randomness of folds
+rf_multiclass_cv <- function(X_train, y_train, X_test, mtry_grid, K = 5, seed = 123) {
+  set.seed(seed)
+  y_train <- as.factor(y_train)
+  folds <- createFolds(y_train, k = K, list = TRUE, returnTrain = FALSE)
+  
+  accuracy_results <- data.frame(mtry = mtry_grid, accuracy = NA)
+  
+  for (i in seq_along(mtry_grid)) {
+    mtry_val <- mtry_grid[i]
+    acc_vec <- numeric(K)
+    
+    for (k in seq_along(folds)) {
+      test_idx <- folds[[k]]
+      train_idx <- setdiff(seq_along(y_train), test_idx)
+      
+      X_tr <- X_train[train_idx, , drop = FALSE]
+      y_tr <- y_train[train_idx]
+      X_val <- X_train[test_idx, , drop = FALSE]
+      y_val <- y_train[test_idx]
+      
+      result <- rf_multiclass(X_tr, y_tr, X_val, mtry = mtry_val)
+      pred_y <- result$pred_y
+      y_val_num <- as.numeric(y_val) - 1
+      
+      acc_vec[k] <- mean(pred_y == y_val_num)
+    }
+    
+    accuracy_results$accuracy[i] <- mean(acc_vec)
+  }
+  
+  # get best mtry
+  best_mtry <- accuracy_results$mtry[which.max(accuracy_results$accuracy)]
+  
+  # train final model and predict on test set
+  final_result <- rf_multiclass(X_train, y_train, X_test, mtry = best_mtry)
+  final_result$best_mtry <- best_mtry
+  
+  return(final_result)
 }
--- scripts/simulation_study.R
+++ scripts/simulation_study.R
@@ -1,8 +1,9 @@
-# Simulation study for S-MPBART
+# Simulation study for S-MPBART, with random forest and MBART with hard splits as benchmarks.
 
 library(mlbench)
 library(openxlsx)
 source("soft_mpbart.R")
+source("random_forest.R")
 
 # ------- WAVEFORM RECOGNITION -------
 
@@ -14,8 +15,7 @@ wave_brier_scores <- rep(0, reps)
 # set seed for reproduceability
 set.seed(123)
 
-# create workbooks to store data and output
-wb <- createWorkbook()
+# create workbook to store output
 wb_output <- createWorkbook()
 
 for (r in 1:reps) {
@@ -23,7 +23,7 @@ for (r in 1:reps) {
   train_wave = mlbench.waveform(300)
   test_wave = mlbench.waveform(500)
   
-  # normalize covariates and define input for S-MPBART
+  # normalize covariates and define input
   wave_X_train <- rank_normalize(as.matrix(train_wave$x))
   wave_y_train <- as.numeric(train_wave$classes) - 1 # convert to 0-based class labels
   wave_X_test  <- rank_normalize(as.matrix(test_wave$x))
@@ -34,26 +34,33 @@ for (r in 1:reps) {
   colnames(wave_X_train) <- paste0("x", 1:p)
   colnames(wave_X_test) <- paste0("x", 1:p)
   
-  wave_train_full <- cbind(wave_X_train, y = wave_y_train)
-  wave_test_full <- cbind(wave_X_test, y = wave_y_test)
+  #wave_train_full <- cbind(wave_X_train, y = wave_y_train)
+  #wave_test_full <- cbind(wave_X_test, y = wave_y_test)
   
   # save data
-  addWorksheet(wb, paste0("rep_", r, "_train"))
-  addWorksheet(wb, paste0("rep_", r, "_test"))
-  writeData(wb, sheet = paste0("rep_", r, "_train"), wave_train_full, colNames = TRUE)
-  writeData(wb, sheet = paste0("rep_", r, "_test"), wave_test_full, colNames = TRUE)
+  #addWorksheet(wb, paste0("rep_", r, "_train"))
+  #addWorksheet(wb, paste0("rep_", r, "_test"))
+  #writeData(wb, sheet = paste0("rep_", r, "_train"), wave_train_full, colNames = TRUE)
+  #writeData(wb, sheet = paste0("rep_", r, "_test"), wave_test_full, colNames = TRUE)
   
+  # ----- SOFT MPBART ------
   # run mcmc
-  mcmc_output <- soft_mpbart(y_train = wave_y_train,
-                             X_train = wave_X_train,
-                             X_test = wave_X_test,
-                             num_classes = 3,
-                             num_burnin = 1500,
-                             num_sim = 1500
-  )
+  #mcmc_output <- soft_mpbart(y_train = wave_y_train,
+                             #X_train = wave_X_train,
+                             #X_test = wave_X_test,
+                             #num_classes = 3,
+                             #num_burnin = 1500,
+                             #num_sim = 1500
+  #)
+  #pred_output <- soft_mpbart_predict(predictions_z = mcmc_output$mu_test_draws)
   
-  # predict
-  pred_output <- soft_mpbart_predict(predictions_z = mcmc_output$mu_test_draws)
+  # ----- RF -----
+  # run rf
+  #p <- ncol(wave_X_train)
+  #mtry_grid <- unique(c(1, floor(sqrt(p)) - 1, floor(sqrt(p)), floor(sqrt(p)) + 1, 5, 10, p))
+  #pred_output <- rf_multiclass_cv(X_train = wave_X_train, y_train = wave_y_train, X_test = wave_X_test, mtry_grid = mtry_grid)
+  
+  # ----- MPBART WITH HARD SPLITS -----
   
   # compute test error rate and brier score
   error <- test_error_rate(y_actual = wave_y_test, y_pred = pred_output$pred_y)
@@ -62,15 +69,15 @@ for (r in 1:reps) {
   wave_brier_scores[r] <- brier_score
 }
 
-# save workbooks
-saveWorkbook(wb, file = "C:\Users\matth\OneDrive\Bureaublad\msc_thesis\thesis_code\data\waveform_data.xlsx", overwrite = TRUE)
+# save data and/or output
+#saveWorkbook(wb, file = "C:/Users/matth/OneDrive/Bureaublad/msc_thesis/thesis_code/data/waveform_data.xlsx", overwrite = TRUE)
 
 addWorksheet(wb_output, "misclassification_rates")
 addWorksheet(wb_output, "brier_scores")
 writeData(wb_output, sheet = "misclassification_rates", x = wave_error_rates)
 writeData(wb_output, sheet = "brier_scores", x = wave_brier_scores)
-saveWorkbook(wb_output, "C:\Users\matth\OneDrive\Bureaublad\msc_thesis\thesis_code\data\smpbart_waveform_output.xlsx", overwrite = TRUE)
-
-
+#saveWorkbook(wb_output, "C:/Users/matth/OneDrive/Bureaublad/msc_thesis/thesis_code/output/smpbart_waveform_output.xlsx", overwrite = TRUE)
+saveWorkbook(wb_output, "C:/Users/matth/OneDrive/Bureaublad/msc_thesis/thesis_code/output/rf_waveform_output.xlsx", overwrite = TRUE)
+#saveWorkbook(wb_output, "C:/Users/matth/OneDrive/Bureaublad/msc_thesis/thesis_code/output/mpbart_waveform_output.xlsx", overwrite = TRUE)
 
 
