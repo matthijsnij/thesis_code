6317624d96db5a4e783a07140125f1458e4c8754
matthijsnij <matthijsnijeboer@hotmail.nl>
2025-06-06 11:20:44 +0200

    Added waveform+noise file, renamed MPBART file.

--- scripts/waveform_noise.R
+++ scripts/waveform_noise.R
@@ -1,22 +1,24 @@
-# Simulation study for S-MPBART, with random forest and MBART with hard splits as benchmarks - WAVEFORM RECOGNITION
+# Simulation study for S-MPBART, with random forest and MBART with hard splits as benchmarks
+# Waveform recognition with noise variables.
 
 library(mlbench)
 library(openxlsx)
 source("soft_mpbart.R")
 source("random_forest.R")
 
-# ------- WAVEFORM RECOGNITION -------
+# ------- WAVEFORM RECOGNITION WITH NOISE VARIABLES -------
 
 # perform 10 replications and save the error rates
 reps <- 10
 wave_error_rates <- rep(0, reps)
 wave_brier_scores <- rep(0, reps)
+num_noise <- 20 # number of noise variables to add
 
 # set seed for reproduceability
 set.seed(123)
 
 # create workbooks
-#wb <- createWorkbook() # for data
+wb <- createWorkbook() # for data
 wb_output <- createWorkbook() # for output
 
 for (r in 1:reps) {
@@ -24,10 +26,17 @@ for (r in 1:reps) {
   train_wave = mlbench.waveform(300)
   test_wave = mlbench.waveform(500)
   
-  # normalize covariates and define input
-  wave_X_train <- rank_normalize(as.matrix(train_wave$x))
+  wave_X_train_real <- as.matrix(train_wave$x)
+  wave_X_test_real <- as.matrix(test_wave$x)
+  
+  wave_X_train_noise <- matrix(rnorm(nrow(wave_X_train_real) * n_noise), nrow = nrow(wave_X_train_real), ncol = n_noise)
+  wave_X_test_noise <- matrix(rnorm(nrow(wave_X_test_real) * n_noise), nrow = nrow(wave_X_test_real), ncol = n_noise)
+  
+  # combine and normalize
+  wave_X_train <- rank_normalize(cbind(wave_X_train_real, wave_X_train_noise))
+  wave_X_test  <- rank_normalize(cbind(wave_X_test_real, wave_X_test_noise))
+  
   wave_y_train <- as.numeric(train_wave$classes) - 1 # convert to 0-based class labels
-  wave_X_test  <- rank_normalize(as.matrix(test_wave$x))
   wave_y_test <- as.numeric(test_wave$classes) - 1 # convert to 0-based class labels
   
   # define column names
@@ -47,11 +56,11 @@ for (r in 1:reps) {
   # ----- SOFT MPBART ------
   # run mcmc
   #mcmc_output <- soft_mpbart(y_train = wave_y_train,
-                             #X_train = wave_X_train,
-                             #X_test = wave_X_test,
-                             #num_classes = 3,
-                             #num_burnin = 1500,
-                             #num_sim = 1500
+  #X_train = wave_X_train,
+  #X_test = wave_X_test,
+  #num_classes = 3,
+  #num_burnin = 1500,
+  #num_sim = 1500
   #)
   #pred_output <- soft_mpbart_predict(predictions_z = mcmc_output$mu_test_draws)
   
@@ -80,5 +89,3 @@ writeData(wb_output, sheet = "brier_scores", x = wave_brier_scores)
 #saveWorkbook(wb_output, "C:/Users/matth/OneDrive/Bureaublad/msc_thesis/thesis_code/output/smpbart_waveform_output.xlsx", overwrite = TRUE)
 saveWorkbook(wb_output, "C:/Users/matth/OneDrive/Bureaublad/msc_thesis/thesis_code/output/rf_waveform_output.xlsx", overwrite = TRUE)
 #saveWorkbook(wb_output, "C:/Users/matth/OneDrive/Bureaublad/msc_thesis/thesis_code/output/mpbart_waveform_output.xlsx", overwrite = TRUE)
-
-
