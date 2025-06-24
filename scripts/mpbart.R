185d8b7c7e00ba61fe3e00437d14e669a633a1a2
matthijsnij <matthijsnijeboer@hotmail.nl>
2025-06-24 16:28:51 +0200

    Progress commit MPBART script

--- scripts/mpbart.R
+++ scripts/mpbart.R
@@ -1,43 +1,50 @@
-# MPBART with hard splits (Xu)
+# MPBART with hard splits (Xu et al (2024))
+
 library(GcompBART)
 
-nb = 1500 # number of burn-in samples
-nd = 1500 # number of post-burn-in posterior draws 
-nt = 200 # number of trees
+# ------ MPBART FUNCTION -------
 
-Prior_mult = function(p, ntree){
-  return(list(nu = p-1+1, 
-              V = diag(p-1),
-              ntrees = ntree,
-              kfac = 2,
-              pswap = 0.1, 
-              pbd = 0.5, 
-              pb = 0.25,
-              alpha = 0.95, 
-              beta = 2.0,
-              nc = 100, 
-              minobsnode = 10))
+mpbart <- function(X_train, # matrix of covariates - training data
+                   y_train, # vector of class labels - training data
+                   X_test, # matrix of covariates - test data
+                   y_test, # vector of class labels - test data
+                   num_burnin = 1000,
+                   num_sim = 1000,
+                   num_trees = 200,
+                   num_classes) {
+   
+  
+  p <- num_classes - 1 # dimension latent
+  
+  # prior settings
+  Prior = list(nu=p+2, # Sigma prior; d.o.f.
+               V= diag(p), # Sigma prior; scale matrix
+               ntrees=200, # number of trees
+               kfac=2.0, # leaf node param prior constant
+               pswap=0,  # prob of swap move in tree prior; set to 0
+               pbd=1.0, # prob of birth/death; set to one
+               pb=0.5 , # prob of birth given birth/death
+               beta = 2.0, # branching process prior
+               alpha = 0.95, # branching process prior
+               nc = 100, # number of equally spaced cutpoints between min and max for each covariate
+               minobsnode = 10) # minimum number of observations in bottom nodes for birth in simulating trees
+  
+  # mcmc settings
+  Mcmc <- list(burn = num_burnin, ndraws = num_sim)
+  
+  df_train <- data.frame(y = y_train, X_train)
+  reference_class <- 0 # reference level
+  
+  model_fit <- model_bart(formula = y ~ ., 
+                      data = df_train, 
+                      type = "multinomial",
+                      base = reference_class,
+                      Prior = Prior,
+                      Mcmc = Mcmc,
+                      correction = FALSE, 
+                      )
+  
+  predict_obj <- predict_bart(obj = model_fit, newdata = as.data.frame(X_test))
+  
 }
 
-Mcmc_mult = function(p,w0 = NULL,sig0 = NULL,nb, nd){
-  res = list(sigma0 = diag(p-1), burn = nb, ndraws = nd, nSigDr = 50)  
-  if(!is.null(w0)){
-    res = append(res, list("w0" = w0), length(res))
-  } 
-  if(!is.null(sig0)){
-    res$sigma0 = sig0
-  }
-  return(res)
-}
-
-fml = "y ~ X1 + X2 + X3 + X4 + X5 + X6"
-
-baseyl = 2 # reference level
-
-p=length(unique(trd$y)) # number of outcome levels
-
-bfit_mult = model_bart(as.formula(fml), data = trd, type = "multinomial",
-                       base = baseyl,
-                       Prior = Prior_mult(p = p, ntree = nt),
-                       Mcmc = Mcmc_mult(p = p,nb = nb, nd = nd),
-                       correction = FALSE, Kindo = FALSE, do_alpha2_prior = FALSE)
